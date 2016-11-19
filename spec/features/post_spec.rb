8c88d32977af5148d68835bb9409dc45223cea92
Bex B <bexb1@hotmail.com>
2016-11-19 17:47:04 +0000

    Added seeds file, new, create and show routes for posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
-describe 'navigate' do 
+describe 'navigate' do
+
   describe 'index page' do
+
     it 'can be reached successfully' do
       visit posts_path
       expect(page.status_code).to eq(200)
@@ -11,6 +13,25 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
+
+  end
+
+
+  describe 'creation' do
+
+    it 'has a new form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      visit new_post_path
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Some rationale'
+      click_on 'Save'
+      expect(page).to have_content('Some rationale')
+    end
+
   end
 
 end
