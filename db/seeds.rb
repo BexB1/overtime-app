e9ddff2acf1714c0dece0e1c65e5ccef75667d62
Becky Bolton <bexb1@hotmail.com>
2017-01-27 17:30:56 +0000

    Added phone attribute to user and made it required.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
-@user = User.create!(:email => "test@test.com", :password => "asdfasdf", :password_confirmation => "asdfasdf", :first_name => "Jon", :last_name => "Snow")
+@user = User.create!(:email => "test@test.com", 
+                    :password => "asdfasdf", 
+                    :password_confirmation => "asdfasdf", 
+                    :first_name => "Jon", 
+                    :last_name => "Snow",
+                    :phone => "555555"
+                    )
 
 puts "1 user created"
 
-AdminUser.create!(:email => "admin@test.com", :password => "asdfasdf", :password_confirmation => "asdfasdf", :first_name => "admin", :last_name => "admin")
+AdminUser.create!(:email => "admin@test.com", 
+                  :password => "asdfasdf", 
+                  :password_confirmation => "asdfasdf", 
+                  :first_name => "admin", 
+                  :last_name => "admin",
+                  :phone => "555555")
 
 puts "1 Admin User created"
 
